c28016a9a8b4fbeeb327463c0895681768fb7b3f
Sankung Sillah <ssillah306@gmail.com>
2023-03-31 04:18:08 -0400

    completed lab

---
+++ index.js
@@ -0,0 +1,13 @@
+const divide = function(){
+    let a = 2000;
+    let b = 100;
+    return (a/b);
+}
+
+const square = (num)=>{
+    return num * num;
+}
+
+const add = (a, b) =>{
+    return a + b;
+}
